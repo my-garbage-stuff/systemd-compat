6efe32b1d17c61d620e0783b40389a2d06ec7cf7
aliriza.keskin <aliriza.keskin@pardus.org.tr>
2023-09-05 09:29:20 +0000

    systemd boot error message bypass

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <iniparser.h>
 #include <service.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 int main(int argc, char **argv){
     if(argv[0] == "systemd"){
         puts("You cannot run service directly!");
         return 1;
+    }
+    struct stat st = {0};
+    if (stat("/some/directory", &st) == -1) {
+        mkdir("/run/systemd/system/", 0700);
     }
     load_service("test");
     //load_service(argv[0]);
